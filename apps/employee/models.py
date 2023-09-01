97df341314190199994ed84deb2e0fcf23bc98f2
Sanowar Dewan <sanowardwnofficial@gmail.com>
2023-09-02 02:04:18 +0600

    circular import error solved

--- apps/employee/models.py
+++ apps/employee/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 from ..company.models import Company
-from ..device.models import Device
 # Create your models here.
 
 class Employee(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
-    devices = models.ManyToManyField(Device, blank=True, through='DeviceAssignment')
 
     class Meta:
         ordering = ['user']
